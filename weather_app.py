8af1d75b1a9533e0d8bf84b7bc21387b2ebafc82
haaris95 <s.muhammedhaaris@gmail.com>
2021-05-04 13:51:22 +0530

    Initial app template

--- weather_app.py
+++ weather_app.py
@@ -1,7 +1,8 @@
 import os
+from time import time
 from pyowm.utils import weather
 from pyowm.utils.formatting import timeformat
-import pytz
+from pytz import timezone
 import pyowm
 import streamlit as st
 from matplotlib import dates
@@ -9,6 +10,19 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 import requests
 import numpy as np
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+
+
+def changeTimeZone(place, utc_time, format):
+    utc_time = datetime.fromtimestamp(utc_time)
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode(place)
+    obj = TimezoneFinder()
+    time_zone = obj.timezone_at(lng=location.longitude, lat=location.latitude)
+    time_formatted = utc_time.astimezone(timezone(time_zone))
+    return time_formatted.strftime(format)
+
 
 
 def bar_chart(days, temp_max, temp_min):
@@ -47,7 +61,7 @@ def preparedata(daily_weather, unit):
     
     for day, temp in daily_weather.items():
         con_day = datetime.strptime(day, "%Y-%m-%d")
-        con_day = con_day.strftime('%m-%d')
+        con_day = con_day.strftime('%b-%d')
         days.append(con_day)
         temp_min.append(float(temp['min']))
         temp_max.append(float(temp['max']))
@@ -113,6 +127,28 @@ def weather_changes(forcaster):
         st.image(clear_weather.weather_icon_url())
     
 
+def otherDetails(forcaster):
+    now = datetime.now()
+    current_weather = forcaster.get_weather_at(now)
+    st.title("Cloud coverage and wind speed")
+    wind_speed = round(current_weather.wind(unit = 'miles_hour')['speed'],2)
+    cloud = current_weather.clouds
+    st.write("The current cloud coverage ", cloud, "%")
+    st.write("The current wind speed is ", wind_speed, " mph")
+    
+
+def timings(place, url):
+    forcast_api = requests.get(url)
+    daily_weather_forcast = (forcast_api.json())
+    st.title("Sunrise and Sunset Times")
+    sunrise = daily_weather_forcast['city']['sunrise']
+    sunset = daily_weather_forcast['city']['sunset']
+    #sunrise = datetime.utcfromtimestamp(sunrise).strftime('%H:%M:%S')
+    #sunset = datetime.utcfromtimestamp(sunset).strftime('%H:%M:%S')
+    sunrise = changeTimeZone(place, sunrise, '%H:%M:%S')
+    sunset = changeTimeZone(place, sunset, '%H:%M:%S')
+    st.write("The sunrise time is ", sunrise)
+    st.write("The sunset time is ", sunset)
 
 
 owm=pyowm.OWM("4ae36ab46c25e5e4911800d4ec047498")
@@ -121,20 +157,30 @@ mgr=owm.weather_manager()
 st.title("5 Day Weather Forecast")
 st.write("### Write the name of a City and select the Temperature Unit and Graph Type from the sidebar")
 
-place=st.text_input("NAME OF THE CITY :", "")
+place = ""
+place = st.text_input("NAME OF THE CITY :", "")
+#st.write('place', place)
 
-if place == None:
+if len(place) == 0 :
     st.write("Input a CITY!")
 unit = st.selectbox("Select Temperature Unit",("Celsius","Fahrenheit"))
 g_type = st.selectbox("Select Graph Type",("Line Graph","Bar Graph"))
 
-url = "http://api.openweathermap.org/data/2.5/forecast?q=" + place + "&appid=4ae36ab46c25e5e4911800d4ec047498"
+if len(place) != 0:
+    url = "http://api.openweathermap.org/data/2.5/forecast?q=" + place + "&appid=4ae36ab46c25e5e4911800d4ec047498"
 
 
-processing(url, g_type, unit)
-forcaster = mgr.forecast_at_place(place, '3h')
-print(forcaster)
-weather_changes(forcaster)
+    processing(url, g_type, unit)
+    forcaster = mgr.forecast_at_place(place, '3h')
+    weather_changes(forcaster)
+    otherDetails(forcaster)
+    timings(place, url)
+now = datetime.now()
+local_now = now.astimezone()
+local_tz = local_now.tzinfo
+local_tzname = local_tz.tzname(local_now)
+print('local_tzname', local_tz)
+
 
 
 
